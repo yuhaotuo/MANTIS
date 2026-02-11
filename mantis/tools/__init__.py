06f1e532037c3382d18d554b8f3b21cfd93a85c5
Bhavay Aggarwal <baggarwal9@atl1-1-03-004-18-2.pace.gatech.edu>
2026-02-11 12:13:58 -0500

    rename

--- mantis/tools/__init__.py
+++ mantis/tools/__init__.py
@@ -1,6 +1,6 @@
 from ._sampler import sample
 from ._region import regional_met
 from ._celltype import celltype_met
-from ._space import spatvar_metabolite
+from ._space import spatvar_met
 from ._genemet import compute_genemet_sci
 from ._spc import compute_spc_ct, compute_spc_sd
